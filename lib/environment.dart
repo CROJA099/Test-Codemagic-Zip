a56c64603b7b78f5cf68693bc7fd299b53f613da
Carlos Rojas <carlos.rojas@foundever.com>
2025-06-03 20:42:10 -0500

    Move keys to environment variables.

---
+++ lib/environment.dart
@@ -0,0 +1,8 @@
+
+
+final class Environment {
+  static const androidApiKey = String.fromEnvironment('ANDROID_APIKEY');
+  static const androidAppId = String.fromEnvironment('ANDROID_APPID');
+  static const iosApiKey = String.fromEnvironment('ANDROID_APIKEY');
+  static const iosAppId = String.fromEnvironment('ANDROID_APPID');
+}
